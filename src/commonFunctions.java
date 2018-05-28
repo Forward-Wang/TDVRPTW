4166791051e13bd205c548bd3eaa4d0a5e408c80
Kashish Bansal <e0302940@nus.edu.sg>
2018-05-28 16:53:25 +0800

    Some erro corrected.. spped to time is now correct..

--- src/commonFunctions.java
+++ src/commonFunctions.java
@@ -3,46 +3,14 @@ public class commonFunctions {
 
 	static int findIndex(double time, double[][] speed, int start, int end) {
 		if(time < speed[0][0])return -1;
-		else if(time > speed[0][speed[0].length-1])return speed[0].length-1;
-//		int index;
-		System.out.println("findIndex: "+start+", "+end);
-//		if (start > end)
-//			return -1;
-//		else {
-//			int middle = (start + end) / 2;
-//			if (speed[0][middle] < time && middle == speed.length - 1) {
-//				return middle;
-//			} else if (speed[0][middle] <= time && speed[0][middle+1] > time) {
-//				return middle;
-//			} else if (speed[0][middle] > time && middle == 0) {
-//				return middle;
-//			} else {
-//				if(middle==start && middle==end)return -1;
-//				int i1 = findIndex(time, speed, start, middle), i2 = findIndex(time, speed, middle + 1, end);
-//				if (i1 != -1) {
-//					System.out.println("returning i1");
-//					return i1;
-//				}
-//				else {					
-//					System.out.println("returning i2");
-//					return i2;
-//				}
-//			}
-//		}
+		else if(time >= speed[0][speed[0].length-1])return speed[0].length-1;
 		
 		int middle = (start+end)/2;
 
 		if(speed[0][middle]==time)return middle;
 	
 		if(start==middle) {
-			if(middle==0) {
-				if(speed[0][middle] > time)return -1;
-				else return middle;
-			}else if(middle == speed[0].length-1) {
-				return middle;
-			}else {
-				return middle;
-			}
+			return middle;
 		}
 		
 		else {
@@ -69,12 +37,15 @@ public class commonFunctions {
 			return -1;
 		}
 
-		if(k != speed[0].length-1) {
-			while(t_ > speed[0][k+1]) {
+		while(k != speed[0].length-1) {
+			if(t_ > speed[0][k+1]) {
 				d-= speed[1][k] * (speed[0][k+1] - t);
 				t = speed[0][k+1];
 				t_ = t + d/speed[1][k+1];
 				k++;
+			}
+			else {
+				break;
 			}
 		}
 		System.out.println("Final time: "+t_);
