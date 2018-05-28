03a38dde4de9c341bac34440311a0746d0c68c5a
Kashish Bansal <e0302940@nus.edu.sg>
2018-05-28 17:50:48 +0800

    Done for today.. Solution file made.. not sure if complete..

--- src/solution.java
+++ src/solution.java
@@ -1,16 +1,33 @@
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class solution {
 	
 	int num_vehicles,num_nodes;
-	vehicle[] vehicles;
+	List<List<Integer>> routes;
 	
 	solution(int num_vehicles, int num_nodes){
 		this.num_nodes = num_nodes;
 		this.num_vehicles = num_vehicles;
+		
+		routes = new ArrayList<List<Integer>>();
+		for(int i=0;i<num_vehicles;i++) {
+			List<Integer> temp_list = new ArrayList<>();
+			temp_list.add(0);
+			routes.add(temp_list);
+		}
 	}
 	
-	void addNodesToVehicle(){
-		
+	public int getNum_vehicles() {
+		return num_vehicles;
 	}
+	
+	public int getNum_nodes() {
+		return num_nodes;
+	}
+
+	void addNodeToVehicle(int vehicle_id, int node_id){
+		routes.get(vehicle_id).add(node_id);
+	}
+
 }
--- src/vehicle.java
+++ src/vehicle.java
@@ -1,4 +1,12 @@
 
 public class vehicle {
-
+	private int id;
+	
+	vehicle(int id){
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
 }
